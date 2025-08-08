c12581726a867f5685cd8737a4bfdb97ccc7b7d4
Andrés Murillo <murilloandrescaleb@gmail.com>
2025-08-07 23:18:41 -0500

    Cambios finales en la rama main

--- task_model.py
+++ task_model.py
@@ -1,16 +1,24 @@
 class TaskModel:
     def __init__(self, task_name):
         self.task_name = task_name
+        self.is_completed = False
 
     def get_task_name(self):
         return self.task_name
     
+    def mark_as_completed(self):
+        self.is_completed = True
+    
     def set_done(self):
-        self.is_done = True
+        self.is_completed = True
+
+    def delete_task(self):
+        self.task_name = None
+        self.is_completed = False
     
     def remove_task(self):
         self.task_name = None
-        self.is_done = False
+        self.is_completed = False
     
-    def is_done(self):
-        return self.is_done
+    def is_completed(self):
+        return self.is_completed
